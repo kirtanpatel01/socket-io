922fedcbf0ae1b51739e6c13d56c1ee3c7566d7c
kirtan0101 <kjpatel200022@gmail.com>
2024-12-27 05:51:41 +0530

    "Removed unused static file serving and updated path for frontend assets."

--- backend/index.js
+++ backend/index.js
@@ -36,8 +36,6 @@ app.use((req, res, next) =>{
     next();
 })
 
-app.use(express.static(path.join(__dirname, "/")));
-
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 app.get("*", (req, res) => {
